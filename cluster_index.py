67d153460bf7d376f0c39541edf3e151ef31ef2d
Serrette Nicolas <serrette.nicola@gmail.com>
2017-06-14 19:37:59 +0200

    Create cluster_index.py

--- cluster_index.py
+++ cluster_index.py
@@ -127,20 +127,3 @@ def Hubert(labels, res):
     return (Nt*yy-((yy+yn)*(yy+ny)))/math.sqrt((yy+yn)*(yy+ny)*(nn+yn)*(nn+ny))
 
 
-with open(sys.argv[1], "r") as labels_file:
-    labels = labels_file.read()
-labels = labels.splitlines()
-
-with open(sys.argv[2], "r") as res_file:
-    res = res_file.read()
-res = res.splitlines()
-
-a, b, c, d = compute_pairs_count(labels, res)
-print("a:"+str(a))
-print("b:"+str(b))
-print("c:"+str(c))
-print("d:"+str(d))
-print("ARI:"+str(ARI(labels, res)))
-print("RI:"+str(RI(labels, res)))
-print("F_measure:"+str(F_measure(labels, res)))
-print("Jaccard:"+str(Jaccard(labels, res)))
