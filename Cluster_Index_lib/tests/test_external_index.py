90a17f399f072d80682200d907bf741ee74a5a45
nicolas.serrette <nicolas.serrette@etu.univ-orleans.fr>
2017-11-14 18:46:18 +0100

    add tests 7/20 passing, some fail with the smallest possible delta

---
+++ Cluster_Index_lib/tests/test_external_index.py
@@ -0,0 +1,95 @@
+import unittest
+from cluster_index_lib import external_index as ei
+import numpy as np
+
+
+# The values used for double checking the results are obtained thanks to the R
+# package clusterCrit. As these values are rounded to the precision of the
+# asserts properties have been adjusted in accordance
+class external_index_test(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(external_index_test, self).__init__(*args, **kwargs)
+        self.labels = np.loadtxt("tests/tests_ressources/Iris/iris.labels")
+        self.results = np.loadtxt("tests/tests_ressources/Iris/iris.results")
+
+    def test_folkes_mallows(self):
+        value = 0.8101679
+        self.assertAlmostEqual(
+            ei.folkes_mallows(self.labels, self.results),
+            value,
+            places=7)
+
+    def test_hubert(self):
+        value = 0.7156547
+        self.assertAlmostEqual(
+            ei.hubert(self.labels, self.results),
+            value,
+            places=7)
+
+    def test_recall(self):
+        value = 0.8190476
+        self.assertAlmostEqual(
+            ei.recall(self.labels, self.results),
+            value,
+            places=7)
+
+    def test_rogers_tanimoto(self):
+        value = 0.7757826
+        self.assertAlmostEqual(
+            ei.rogers_tanimoto(self.labels, self.results),
+            value,
+            places=7)
+
+    def test_russel_rao(self):
+        value = 0.2693512
+        self.assertAlmostEqual(
+            ei.russel_rao(self.labels, self.results),
+            value,
+            places=7)
+
+    def test_ri(self):
+        value = 0.873736
+        self.assertAlmostEqual(
+            ei.ri(self.labels, self.results),
+            value,
+            places=6)
+
+    def test_jaccard(self):
+        value = 0.6808414
+        self.assertAlmostEqual(
+            ei.jaccard(self.labels, self.results),
+            value,
+            places=7)
+
+    def test_kulczynski(self):
+        value = 0.8102161
+        self.assertAlmostEqual(
+            ei.kulczynski(self.labels, self.results),
+            value,
+            places=7)
+
+    def test_precision(self):
+        value = 0.8013844
+        self.assertAlmostEqual(
+            ei.precision(self.labels, self.results),
+            value,
+            places=7)
+
+    def test_f_measure(self):
+        value = 0.8101197
+        self.assertAlmostEqual(
+            ei.f_measure(self.labels, self.results),
+            value,
+            places=7)
+
+    def test_phi(self):
+        value = 2.582312e-08
+        self.assertAlmostEqual(
+            ei.phi(self.labels, self.results),
+            value,
+            places=6)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ Cluster_Index_lib/tests/test_internal_index.py
@@ -0,0 +1,81 @@
+import unittest
+from cluster_index_lib import internal_index as ii
+from sklearn import datasets
+
+
+# The values used for double checking the results are obtained thanks to the R
+# package clusterCrit. As these values are rounded to the precision of the
+# asserts properties have been adjusted in accordance
+class internal_index_test(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(internal_index_test, self).__init__(*args, **kwargs)
+        iris = datasets.load_iris()
+        self.data = iris.data
+        self.labels = iris.target
+
+    def test_silhouette(self):
+        value = 0.5032507
+        self.assertAlmostEqual(
+            ii.silhouette(self.labels, self.data),
+            value,
+            places=7)
+
+    def test_ray_turi(self):
+        value = 0.226929
+        self.assertAlmostEqual(
+            ii.ray_turi(self.labels, self.data),
+            value,
+            places=7)
+
+    def test_tau(self):
+        value = 0.5842415
+        self.assertAlmostEqual(
+            ii.tau(self.labels, self.data),
+            value,
+            places=7)
+
+    def test_xie_beni(self):
+        value = 11.91824
+        self.assertAlmostEqual(
+            ii.xie_beni(self.labels, self.data),
+            value,
+            places=5)
+
+    def test_gamma(self):
+        value = 0.8792793
+        self.assertAlmostEqual(
+            ii.gamma(self.labels, self.data),
+            value,
+            places=7)
+
+    def test_ball_hall(self):
+        value = 0.595912
+        self.assertAlmostEqual(
+            ii.ball_hall(self.labels, self.data),
+            value,
+            places=6)
+
+    def test_calinski_harabasz(self):
+        value = 486.3208
+        self.assertAlmostEqual(
+            ii.calinski_harabasz(self.labels, self.data),
+            value,
+            places=4)
+
+    def test_log_ss_ratio(self):
+        value = 1.889583
+        self.assertAlmostEqual(
+            ii.log_ss_ratio(self.labels, self.data),
+            value,
+            places=6)
+
+    def test_c_index(self):
+        value = 0.04680377
+        self.assertAlmostEqual(
+            ii.c_index(self.labels, self.data),
+            value,
+            places=8)
+
+
+if __name__ == '__main__':
+    unittest.main()
